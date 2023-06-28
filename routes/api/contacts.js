1aedd967b9831d60762d9bf421de95d83e7b6a99
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-06-28 11:49:17 +0300

    conect to mongoDB finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,33 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const validateBody = require("../../middlewares/validateBody");
+const isValidId = require("../../middlewares/isValidId");
 
 const router = express.Router();
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.removeContact);
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateFavorite
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateContact
+);
 
 module.exports = router;
