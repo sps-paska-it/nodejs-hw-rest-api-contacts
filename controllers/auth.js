80016c0847e4daf2560c06d5f9036776cbc8767a
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-07-03 01:12:48 +0300

    token next

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,40 @@
+const bcrypt = require("bcrypt");
+
 const { User } = require("../models/user");
 
-// const HttpError = require("../helpers/HttpError");
+const HttpError = require("../helpers/HttpError");
 const ctrlWrapper = require("../helpers/ctrlWrapper");
 
 const register = async (req, res) => {
-  const newUser = await User.create(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
+
+  const hashPassword = await bcrypt.hashPassword(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.json({
     email: newUser.email,
     name: newUser.name,
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  //   const token = "";
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,8 +1,7 @@
 const handleMongooseError = (error, data, next) => {
   const { name, code } = error;
-  error.status(400);
-  console.log(name);
-  console.log(code);
+  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  error.status = status;
   next();
 };
 
